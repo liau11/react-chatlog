9855407e79ab703928d8f77c4cd0aaef92dbd4cf
Lily <aulily09@gmail.com>
2023-06-14 20:56:38 -0700

    Using state to toggle like button to change color

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const [like, pressLike] = useState(false);
+
+  const toggleLike = () => {
+    pressLike(!like)
+  }
+
+  const heartColor = like ? '❤️' : '🤍';
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
         <p className="entry-time"><TimeStamp timeStamp={props.timeStamp} /></p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={toggleLike}>{heartColor}</button>
       </section>
     </div>
   );
 };
-
 ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
